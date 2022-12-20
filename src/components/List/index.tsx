55d2151cee7c8cdbfee22dec18b77db8114fd064
victorluadev <streamyodin@gmail.com>
2022-12-19 23:23:25 -0300

    feat: Dinamyc list render

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 
 function List() {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00"
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00"
+    },
+    {
+      task: "typescript",
+      time: "00:30:00"
+    }
+  ];
+
   return (
     <aside>
       <h2> Estudos do dia </h2>
       <ul>
-        <li>
-          <h3>
-            React
-          </h3>
-          <span>
-            02:00:00
-          </span>
-        </li>
-        <li>
-          <h3>
-            Javascript
-          </h3>
-          <span>
-            01:00:00
-          </span>
-        </li>
+        { tasks.map((item, index) => (
+            <li key={index}>
+              <h3>
+                {item.task}
+              </h3>
+              <span>
+                {item.time}
+              </span>
+            </li>
+          ))
+        }
+        
       </ul>
     </aside>
   )
