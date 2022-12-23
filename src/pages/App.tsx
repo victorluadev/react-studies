bd7c1b7c7d50c0972712bfca2465c38c461e2319
victorluadev <streamyodin@gmail.com>
2022-12-22 23:43:38 -0300

    feat: Adicionando cronômetro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
+import Timer from '../components/Timer';
 
 import style from './App.module.scss';
 
@@ -9,6 +10,7 @@ function App() {
     <div className={style.AppStyle}>
       <Form />
       <List />
+      <Timer />
     </div>
   );
 }
