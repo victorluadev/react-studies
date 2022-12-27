724df96791346a610996e7bee713cbf1343107a3
victorluadev <streamyodin@gmail.com>
2022-12-26 23:32:47 -0300

    feat: Adicionando states

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 import style from './List.module.scss';
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       time: "02:00:00"
@@ -17,11 +17,13 @@ function List() {
       task: "typescript",
       time: "00:30:00"
     }
-  ];
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, { task: "Estudar states", time: "05:00:00"}]);
+      }}> Estudos do dia </h2>
       <ul>
         { tasks.map((item, index) => (
             <Item 
